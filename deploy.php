e3496f16d29c18b88a61aefa84cddd4e1595c8b8
Johannes Berdin <ich@johannesberdin.de>
2016-09-27 16:06:55 +0200

    Build for GitHub Pages

--- deploy.php
+++ deploy.php
@@ -6,5 +6,5 @@
 
 task('github', function() {
   writeln('Deploy to GitHub pages');
-  runLocally("git subtree push --prefix build_production origin gh-pages");
+  runLocally("git subtree push --prefix build_github origin gh-pages");
 })->desc('Deploy the site to GitHub pages.');
