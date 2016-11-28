c1b994c0b58cb2132bfa7f6869d50194fd9d1cd0
Johannes Berdin <ich@johannesberdin.de>
2016-11-28 14:42:35 +0100

    Finalized content creation

--- js/main.js
+++ js/main.js
@@ -10889,14 +10889,8 @@ https://github.com/imakewebthings/waypoints/blob/master/licenses.txt
 window.$ = window.jQuery = require('jquery');
 require('waypoints/lib/jquery.waypoints.js');
 
-$(document).ready(function () {
-    $.getJSON('https://api.instagram.com/v1/users/1532025072/media/recent/?access_token=1532025072.d9be861.695ee38866a0488da03ffec00d998ee4&count=10&callback=?', function (response) {
-        if (response.data) {
-            $.each(response.data, function (key, val) {
-                $('#instagram').append('<div class="feed"><a target="_blank" href="' + val.link + '"><img src="' + val.images.standard_resolution.url + '"></a></div>');
-            });
-        }
-    });
+(function () {
+    "use strict";
 
     $('.fadeInLeft, .fadeInRight, .fadeInUp').css('opacity', 0).waypoint(function () {
         var $el = $(this.element);
@@ -10904,7 +10898,34 @@ $(document).ready(function () {
     }, {
         offset: '100%'
     });
-});
+
+    /**
+     * Track outbound links in Google Analytics
+     * Adapted from: https://www.sitepoint.com/track-outbound-links-google-analytics/
+     */
+    $('a').on('click', function (e) {
+        var link = $(this);
+
+        // Link already aborted?
+        if (e.isDefaultPrevented() || typeof ga !== "function") return;
+        if (link.length != 1 || window.location.host == link[0].host) return;
+
+        // Record outbound link
+        e.preventDefault();
+        var href = link[0].href;
+        ga('send', 'event', 'outbound', 'link', href, {
+            'hitCallback': loadPage
+        });
+
+        // Redirect after one millisecond
+        setTimeout(loadPage, 100);
+
+        // Redirect to outbound page
+        function loadPage() {
+            document.location = href;
+        }
+    });
+})(jQuery);
 
 },{"jquery":1,"waypoints/lib/jquery.waypoints.js":2}]},{},[3]);
 
